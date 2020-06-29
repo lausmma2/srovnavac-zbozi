6f2b60aa2db9f3aadcbea024ebffdc6dcf2076d6
Marek <marek.lausman@gmail.com>
2020-06-29 14:09:49 +0200

    Updates

---
+++ precache-manifest.f9f47243b819b3230326dc0ab36c9f8a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "548197b7ba2830d2faf3a9503b6ac0de",
+    "url": "/srovnavac-zbozi/index.html"
+  },
+  {
+    "revision": "7de76dfab937aa81f577",
+    "url": "/srovnavac-zbozi/static/css/2.2c11666b.chunk.css"
+  },
+  {
+    "revision": "1ecafb775d387f65f74d",
+    "url": "/srovnavac-zbozi/static/css/main.60f9e1d7.chunk.css"
+  },
+  {
+    "revision": "7de76dfab937aa81f577",
+    "url": "/srovnavac-zbozi/static/js/2.1f167287.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/srovnavac-zbozi/static/js/2.1f167287.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ecafb775d387f65f74d",
+    "url": "/srovnavac-zbozi/static/js/main.33606d33.chunk.js"
+  },
+  {
+    "revision": "55fa9dd700dcefd9a0e7",
+    "url": "/srovnavac-zbozi/static/js/runtime-main.2baefcee.js"
+  }
+]);
